cdfcf7de07ead22df5c391223a31bc8d63b38d10
Sydney Sprouse <sydneysprouse@Sydneys-MacBook-Air.local>
2017-05-25 20:52:31 -0600

    working copy

---
+++ js/animations.js
@@ -0,0 +1,13 @@
+// var textarea = $('#main-compose'),
+//      tweetControls = $('#tweet-controls'),
+//      charCount = $('#char-count'),
+//      tweetSubmit = $('#tweet-submit'),
+//      charLeft,
+//      tweet = $('.tweet'),
+//      tweetCompose = $('.tweet-compose'),
+//      tweetActions = $('.tweet-actions');
+//
+// $(".tweet-compose").on("click", function() {
+//   $(this).css({'height': '5.0em'});
+//   $('#tweet-controls').show();
+// });
